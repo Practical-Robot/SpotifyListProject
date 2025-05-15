9c7631823690d9781ace26e30719cc62d7c838af
Robin <robin@prpractical.ca>
2025-05-14 21:23:14 -0600

    Added static Elements to SearchBar

--- spotify-lister/src/App.js
+++ spotify-lister/src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
+import Tracklist from "./modules/Tracklist";
 
 
 function App() {
   
+
+  return(
+    <div>
+      <header>
+        Ja<span className="accentLetters">mmm</span>ing
+      </header>
+      <SearchBar />
+      <SearchResults />
+      <Playlist />
+      <Tracklist />
+      <Track />
+    </div>
+    
+  )
 }
 
 export default App;
--- spotify-lister/src/modules/SearchBar.js
+++ spotify-lister/src/modules/SearchBar.js
@@ -2,6 +2,21 @@ import React from "react";
 
 function SearchBar(){
 
+return(
+    <div>
+        <form>
+        <label htmlFor="searchBox"></label>
+            <input 
+            id="searchBox"
+            name="searchBox"
+            type="text"
+            placeholder="Singing in the Rain"
+            />
+            <input type="submit" value="Search"/>
+            </form>
+    </div>
+)
+
 }
 
 export default SearchBar;
