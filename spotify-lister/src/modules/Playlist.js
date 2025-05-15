ceae8dbc252626051258b92cba4e17084a88be72
Robin <robin@prpractical.ca>
2025-05-14 21:36:56 -0600

    Playlist Static Info

--- spotify-lister/src/modules/Playlist.js
+++ spotify-lister/src/modules/Playlist.js
@@ -1,7 +1,34 @@
 import React from "react";
 
-function SearchBar() {
-
+function Playlist() {
+    
+    return(
+    <div>
+        <ul>
+            <li>
+                Song: Intergalactic
+                <br />
+                Artist: Beastie Boys
+                <br />
+                Album: Hello Nasty
+            </li>
+            <li>
+                Song: I Can Do It With a Broken Heart
+                <br />
+                Artist: Taylor Swift
+                <br />
+                Album: THE TORTURED POET
+            </li>
+            <li>
+                Song: Blah Blah Blah
+                <br />
+                Artist: Armin van Buuren
+                <br />
+                Album: Balance
+            </li>
+        </ul>
+    </div>
+    );
 }
 
-export default SearchBar;
+export default Playlist;
--- spotify-lister/src/modules/SearchResults.js
+++ spotify-lister/src/modules/SearchResults.js
@@ -2,6 +2,34 @@ import React from 'react';
 
 function SearchResults(){
 
+    return(
+        <div>
+            <ul>
+                <li>
+                    Song: Singing in the Rain
+                    <br />
+                    Artist: Dick Van Dyke
+                </li>
+                                <li>
+                    Song: B.Y.O.B.
+                    <br />
+                    Artist: System of a Down
+                </li>
+                                <li>
+                    Song: Lick my Lovepump
+                    <br />
+                    Artist: Spinal Tap
+                </li>
+                                <li>
+                    Song: Love in an Elevator
+                    <br />
+                    Artist: Aerosmith
+                </li>
+            </ul>
+
+        </div>
+    )
+
 }
 
 export default SearchResults;
