9f05a09f1388b8ca378e94693e864a008fd3c9ad
Robin <robin@prpractical.ca>
2025-05-14 21:26:37 -0600

    imports to APP

--- spotify-lister/src/App.js
+++ spotify-lister/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Tracklist from "./modules/Tracklist";
-
+import SearchResults from "./modules/SearchResults";
+import SearchBar from "./modules/SearchBar";
+import Playlist from "./modules/Playlist";
+import Track from "./modules/Track";
 
 function App() {
   
