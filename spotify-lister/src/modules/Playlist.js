191a2ab5717105ada6cfaca9c4cc144404755e4b
Robin <robin@prpractical.ca>
2025-05-14 21:55:32 -0600

    Static Lists

--- spotify-lister/src/modules/Playlist.js
+++ spotify-lister/src/modules/Playlist.js
@@ -4,29 +4,34 @@ function Playlist() {
     
     return(
     <div>
-        <ul>
-            <li>
-                Song: Intergalactic
-                <br />
-                Artist: Beastie Boys
-                <br />
-                Album: Hello Nasty
-            </li>
-            <li>
-                Song: I Can Do It With a Broken Heart
-                <br />
-                Artist: Taylor Swift
-                <br />
-                Album: THE TORTURED POET
-            </li>
-            <li>
-                Song: Blah Blah Blah
-                <br />
-                Artist: Armin van Buuren
-                <br />
-                Album: Balance
-            </li>
-        </ul>
+        <form>
+            <input type="text" value='Cool Playlist'/>
+            <ul>
+                <li>
+                    Song: Intergalactic
+                    <br />
+                    Artist: Beastie Boys
+                    <br />
+                    Album: Hello Nasty
+                </li>
+                <li>
+                    Song: I Can Do It With a Broken Heart
+                    <br />
+                    Artist: Taylor Swift
+                    <br />
+                    Album: THE TORTURED POET
+                </li>
+                <li>
+                    Song: Blah Blah Blah
+                    <br />
+                    Artist: Armin van Buuren
+                    <br />
+                    Album: Balance
+                </li>
+            </ul>
+
+            <input type='submit' value='Save to Spotify'/>
+        </form>
     </div>
     );
 }
